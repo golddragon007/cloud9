fa86c9212d8f7b8789b1cd70ddeb43c992c36a0e
Sebastian <sebastian.colomar@ext.ec.europa.eu>
2018-03-28 10:49:58 +0000

    finished

--- toolkit.sh
+++ toolkit.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -x
 set -e
-DIR=$HOME/environment
+DIR=$HOME/environment;CLOUD=$DIR/cloud9;CONF=$CLOUD/conf.d;KONF=$CONF/cloud9.conf
 case $2 in
-clean|clone) for x in {0..3};do source $DIR/cloud9/toolkit.$x;done;;
-purge) source $DIR/cloud9/toolkit.0;rm -rf $DIR/$REPO $DIR/cloud9/conf.d/$SITE.conf;;
-enable) source $DIR/cloud9/toolkit.0;sudo sed -i "/build/s/environment.*build/environment\/$REPO\/build/" $HTTP;sudo service httpd restart;echo You can access your website through this URL\:;echo;awk '/^project.url.base/{print $3}' $DIR/$REPO/$FILE;;
+clean) $CLOUD/system.sh;$CLOUD/native.sh;$CLOUD/configure.sh $1;$CLOUD/install.sh $1 clean;;
+clone) $CLOUD/system.sh;$CLOUD/native.sh;$CLOUD/configure.sh $1;$CLOUD/clone.sh $1;$CLOUD/install.sh $1 clone;;
+purge) source $CONF/$1.conf;rm -rf $DIR/$REPO $CONF/$1.conf;; 
+enable) source $KONF;source $CONF/$1.conf;sudo sed -i "/build/s/environment.*build/environment\/$REPO\/build/" $HTTP;sudo service httpd restart;echo You can access your website through this URL\:;echo;awk '/^project.url.base/{print $3}' $DIR/$REPO/$FILE;;
 disable) sudo service httpd stop;;
+*) $CLOUD/system.sh;;
 esac
