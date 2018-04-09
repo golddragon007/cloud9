af2bc62e471094a70f932c3f5317499cb4b5514a
EC2 Default User <ec2-user@ip-172-31-44-9.eu-west-1.compute.internal>
2018-04-09 17:34:09 +0000

    delete-lines-from-basic.sh

--- basic.sh
+++ basic.sh
@@ -23,9 +23,3 @@ git config --global alias.dc 'diff --cached'
 git config --global alias.lg 'log -p'
 git config --global alias.bra 'branch -a'
 fi
-#!/bin/sh
-set -e
-BASHRC=$HOME/.bashrc;grep -q "added by DevOps" $BASHRC||(echo '# added by DevOps'>>$BASHRC;echo HISTSIZE=1000000>>$BASHRC;echo HISTFILESIZE=1000000>>$BASHRC;echo 'HISTTIMEFORMAT="%F %T "'>>$BASHRC;source $BASHRC)
-CLOUD=$HOME/environment/cloud9;CONF=$CLOUD/conf.d;[ -d $CONF ]||mkdir -p $CONF;KONF=$CONF/cloud9.conf
-sudo sysctl -p $CLOUD/sysctl.conf 
-BIN=/usr/bin;LOCAL=/usr/local/bin;sudo yum update -y;ls $BIN/composer||(sudo curl -sS https://getcomposer.org/installer|sudo php;sudo mv composer.phar $LOCAL/composer;sudo ln -s $LOCAL/composer $BIN/composer)
