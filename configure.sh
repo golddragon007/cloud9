de2fb0d3e33902e470cbe795cb16e5bca8c73a96
S <sc@gmail.com>
2018-04-11 23:39:08 +0000

    minor

--- configure.sh
+++ configure.sh
@@ -4,12 +4,12 @@ CLOUD=$(dirname "$(readlink -f "$0")");CONFD=$CLOUD/conf.d;CONF=$CONFD/$1.conf;D
 if [ ! -f $CONF ];then
 read -p "SITE (default name: $1) = " SITE;SITE=${SITE:-"$1"};echo SITE=$SITE>>$CONF
 read -p "REPS (y/n) = " REPS;REPS=${REPS:-n};echo REPS=$REPS>>$CONF
-read -p "VERSION (2.x.x: it is better not to leave the default) = " VERSION;echo VERSION=$VERSION>>$CONF
-read -p "ASDA (password: please leave it blank for a new subsite) = " ASDA;ASDA=${ASDA:-NOPASSWORD};echo ASDA=$ASDA>>$CONF
+read -p "VERSION (2.3.x: please change it if the default value is not appropriate) = " VERSION;echo VERSION=$VERSION>>$CONF
+read -p "ASDA (password: please leave it blank ONLY for a new subsite) = " ASDA;ASDA=${ASDA:-NOPASSWORD};echo ASDA=$ASDA>>$CONF
 read -p "URL (default https://ec.europa.eu/$SITE) = " URL;URL=${URL:-"https://ec.europa.eu/$SITE"};echo URL=$URL>>$CONF
 REP0=$([ $REPS = y ]&&echo reps-$SITE-reference||echo $SITE-reference);read -p "REPO (default $REP0) = " REPO;REPO=${REPO:-"$REP0"};echo REPO=$REPO>>$CONF
-read -p "GITHUB REPO (default https://github.com/ec-europa/$REPO.git but you can choose SSH or change the URL) = " HTTPS;HTTPS=${HTTPS:-"https://github.com/ec-europa/$REPO.git"};echo HTTPS=$HTTPS>>$CONF
-read -p "BRANCH (default new branch name toolkit/upgrade: please change it if necessary but please be careful so that the branch has the new toolkit) = " BRANCH;BRANCH=${BRANCH:-"toolkit/upgrade"};echo BRANCH=$BRANCH>>$CONF
+read -p "GITHUB REPO (https://github.com/ec-europa/$REPO.git please change the default if necessary or choose SSH) = " HTTPS;HTTPS=${HTTPS:-"https://github.com/ec-europa/$REPO.git"};echo HTTPS=$HTTPS>>$CONF
+read -p "BRANCH (toolkit/upgrade: please change the default value if necessary but please be careful so that the branch has the new toolkit) = " BRANCH;BRANCH=${BRANCH:-"toolkit/upgrade"};echo BRANCH=$BRANCH>>$CONF
 alias drush="$DIR/$REPO/toolkit/drush -r build";echo alias $(alias drush)>>$CONF
 alias phing="$DIR/$REPO/toolkit/phing";echo alias $(alias phing)>>$CONF
 fi
--- system.sh
+++ system.sh
@@ -5,5 +5,5 @@ BASH=bashrc.conf;echo 'FILE='$BASH';grep -q $FILE $HOME/.bashrc||echo "source \$
 SYS=sysctl.conf;sudo cp $SYS /etc/sysctl.d/99-$SYS;sudo sysctl -p $CLOUD/$SYS 
 SSH=$HOME/.ssh/authorized_keys;PUB=$CLOUD/devops.pub;grep -q devops $SSH||(echo "#DevOps key:">>$SSH;cat $PUB>>$SSH)
 BIN=/usr/bin;LOCAL=/usr/local/bin;sudo yum update -y;ls $BIN/composer||(sudo curl -sS https://getcomposer.org/installer|sudo php;sudo mv composer.phar $LOCAL/composer;sudo ln -s $LOCAL/composer $BIN/composer)
-curl http://downloads.drone.io/release/linux/amd64/drone.tar.gz|tar zx;sudo install -t /usr/local/bin drone
+curl http://downloads.drone.io/release/linux/amd64/drone.tar.gz|tar zx;sudo install -t /usr/local/bin drone;rm drone
 #crontab -l|grep -q growpart||(echo "* * * * * sudo growpart /dev/xvda 1;sudo resize2fs /dev/xvda1"|sudo tee -a /var/spool/cron/ec2-user;sudo chown ec2-user. /var/spool/cron/ec2-user;sudo chmod 600 /var/spool/cron/ec2-user)
