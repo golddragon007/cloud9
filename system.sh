5e6adc6c058980190f09ec21ed74c3a5fa569be0
Sebastian <sebastian.colomar@ext.ec.europa.eu>
2018-04-11 13:27:31 +0000

    fix typo in BASHRC which prevented to change HISTORY configuration

--- system.sh
+++ system.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 CLOUD=$(dirname "$(readlink -f "$0")");CONFD=$CLOUD/conf.d;[ -d $CONFD ]||mkdir -p $CONFD;CONF=$CONFD/cloud9.conf
-BASH=bashrc.conf;echo 'FILE='$BASHRC';grep -q $FILE $HOME/.bashrc||echo "source \$HOME/.$FILE"|tee -a $HOME/.bashrc'|sudo tee /etc/profile.d/$BASH;cp $CLOUD/$BASH $HOME/.$BASH;source $CLOUD/$BASH
+BASH=bashrc.conf;echo 'FILE='$BASH';grep -q $FILE $HOME/.bashrc||echo "source \$HOME/.$FILE"|tee -a $HOME/.bashrc'|sudo tee /etc/profile.d/$BASH;cp $CLOUD/$BASH $HOME/.$BASH;source $CLOUD/$BASH
 SYS=sysctl.conf;sudo cp $SYS /etc/sysctl.d/99-$SYS;sudo sysctl -p $CLOUD/$SYS 
 SSH=$HOME/.ssh/authorized_keys;PUB=$CLOUD/devops.pub;grep -q devops $SSH||(echo "#DevOps key:">>$SSH;cat $PUB>>$SSH)
 BIN=/usr/bin;LOCAL=/usr/local/bin;sudo yum update -y;ls $BIN/composer||(sudo curl -sS https://getcomposer.org/installer|sudo php;sudo mv composer.phar $LOCAL/composer;sudo ln -s $LOCAL/composer $BIN/composer)
