ea187d2dc376d46f6d190ae6e74c41ec9c703599
Sebastian <sebastian.colomar@ext.ec.europa.eu>
2018-04-12 00:43:38 +0000

    add source to get DIR variable

--- cloud9.sh
+++ cloud9.sh
@@ -10,7 +10,7 @@ s) $CLOUD/system.sh;$CLOUD/basic.sh;;
 c) [ ! -f $CONF ]&&($CLOUD/system.sh;$CLOUD/basic.sh);$CLOUD/native.sh;$CLOUD/configure.sh $OPTARG;$CLOUD/clone.sh $OPTARG;$CLOUD/install.sh $OPTARG clone;;
 n) [ ! -f $CONF ]&&($CLOUD/system.sh;$CLOUD/basic.sh);$CLOUD/native.sh;$CLOUD/configure.sh $OPTARG;$CLOUD/install.sh $OPTARG clean;;
 e) source $CONF;source $CONFD/$OPTARG.conf;sudo service httpd restart;echo You can access your website through this URL\:;awk '/^project.url.base/{print $3}' $DIR/$REPO/$FILE;;
-r) source $CONFD/$OPTARG.conf;rm -rf $DIR/$REPO $CONFD/$OPTARG.conf;;
+r) source $CONF;source $CONFD/$OPTARG.conf;rm -rf $DIR/$REPO $CONFD/$OPTARG.conf;;
 :) echo "The option -$OPTARG requires the name of the subsite as an argument";;
 \?) echo "Invalid option: -$OPTARG";;
 esac
