71b1ae151abda397d56491006cf42e7107149b40
gervasek <gervasek@users.noreply.github.com>
2018-04-12 22:37:29 +0200

    Update aliases.sh

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -9,3 +9,5 @@ alias hgrep='history | grep'
 alias cdenv="cd ~/environment/"
 
 alias cloud9GetPublicIP="curl http://169.254.169.254/latest/meta-data/public-ipv4 && echo ''"
+
+alias cloud9EC2Reboot='aws ec2 reboot-instances --instance-ids="$(curl http://169.254.169.254/latest/meta-data/instance-id)"'
