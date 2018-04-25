b7e1d1f88fd292ddc1ad5518a2b2fb9601986760
sc <sc>
2018-04-25 14:12:38 +0000

    fix broken link

--- lib/minimal.sh
+++ lib/minimal.sh
@@ -3,6 +3,6 @@ set -e
 sudo yum -y remove '*mysql*' '*php*' '*httpd*' '*nodejs*' '*kernel*' nano '*emacs*';sudo yum -y update
 BIN=/usr/bin;LOCAL=/usr/local/bin;
 	if [ ! -f $BIN/drone ];then
-	curl https://github.com/drone/drone-cli/releases/download/v0.8.5/drone_linux_amd64.tar.gz|tar zx;
+	curl http://downloads.drone.io/release/linux/amd64/drone.tar.gz|tar zx;
 	sudo install -t /usr/local/bin drone;rm drone;sudo ln -s $LOCAL/drone $BIN/drone
   fi
