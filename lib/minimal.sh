878d045b2288048d1b0ddc1a95c080d46935a450
Gregory Boddin <gboddin@users.noreply.github.com>
2018-04-26 12:23:45 +0200

    Update minimal.sh

--- lib/minimal.sh
+++ lib/minimal.sh
@@ -2,7 +2,9 @@
 set -e
 sudo yum -y remove '*mysql*' '*php*' '*httpd*' '*nodejs*' '*kernel*' nano '*emacs*' '*rpc*';sudo yum -y update
 BIN=/usr/bin;LOCAL=/usr/local/bin;
-	if [ ! -f $BIN/drone ];then
-	curl http://downloads.drone.io/release/linux/amd64/drone.tar.gz|tar zx;
+
+if [ ! -f $BIN/drone ];then
+	curl -L https://github.com/drone/drone-cli/releases/download/v0.8.5/drone_linux_amd64.tar.gz|tar zx;
 	sudo install -t /usr/local/bin drone;rm drone;sudo ln -s $LOCAL/drone $BIN/drone
-  fi
+fi
+sudo pip install docker-compose --upgrade
