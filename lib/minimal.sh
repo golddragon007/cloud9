7c7904740604099bd1b6019936325bb7064cfb12
sc <sc>
2018-04-29 18:06:22 +0000

    upgrade pip

--- lib/minimal.sh
+++ lib/minimal.sh
@@ -2,9 +2,9 @@
 set -e
 sudo yum -y remove '*mysql*' '*php*' '*httpd*' '*nodejs*' '*kernel*' nano '*emacs*' '*rpc*';sudo yum -y update
 BIN=/usr/bin;LOCAL=/usr/local/bin;
-
-if [ ! -f $BIN/drone ];then
-	curl -L https://github.com/drone/drone-cli/releases/download/v0.8.5/drone_linux_amd64.tar.gz|tar zx;
-	sudo install -t /usr/local/bin drone;rm drone;sudo ln -s $LOCAL/drone $BIN/drone
-fi
+	if [ ! -f $BIN/drone ];then
+		curl -L https://github.com/drone/drone-cli/releases/download/v0.8.5/drone_linux_amd64.tar.gz|tar zx;
+		sudo install -t /usr/local/bin drone;rm drone;sudo ln -s $LOCAL/drone $BIN/drone
+	fi
+sudo pip install --upgrade pip
 sudo pip install docker-compose --upgrade
