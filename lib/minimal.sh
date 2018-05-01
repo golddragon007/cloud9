918e2ba3f4abf7048f7c81a4d284b90f8b3a1490
secobau <sebastian.colomar@gmail.com>
2018-05-02 00:31:18 +0200

    install docker-compose without pip

--- lib/minimal.sh
+++ lib/minimal.sh
@@ -2,9 +2,16 @@
 set -e
 sudo yum -y remove '*mysql*' '*php*' '*httpd*' '*nodejs*' '*kernel*' nano '*emacs*' '*rpc*';sudo yum -y update
 BIN=/usr/bin;LOCAL=/usr/local/bin;
-	if [ ! -f $BIN/drone ];then
-		curl -L https://github.com/drone/drone-cli/releases/download/v0.8.5/drone_linux_amd64.tar.gz|tar zx;
-		sudo install -t /usr/local/bin drone;rm drone;sudo ln -s $LOCAL/drone $BIN/drone
+	TOOL=drone;
+	if [ ! -f $BIN/$TOOL ];then
+		curl -L https://github.com/drone/drone-cli/releases/download/v0.8.5/$TOOL_linux_amd64.tar.gz|tar zx;
+		sudo install -t $LOCAL $TOOL;rm $TOOL;
+		sudo ln -s $LOCAL/$TOOL $BIN/$TOOL
 	fi
-sudo pip install --upgrade pip
-sudo pip install docker-compose --upgrade
+	TOOL=docker-compose;
+	if [ ! -f $BIN/$TOOL ];then
+		COMPOSE=1.21.1
+		URL="https://github.com/docker/compose/releases/download/$COMPOSE/$TOOL-$(uname -s)-$(uname -m)"
+		sudo curl -L $URL -o $LOCAL/$TOOL;
+		sudo ln -s $LOCAL/$TOOL $BIN/$TOOL
+	fi
