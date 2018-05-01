9db60169edbbd266b4d440c40f7bdf0d19bd5efd
secobau <sebastian.colomar@gmail.com>
2018-05-02 01:06:26 +0200

    check installed versions

--- lib/lamp.sh
+++ lib/lamp.sh
@@ -8,19 +8,20 @@ fi
 YUM=/tmp/yum.list;sudo yum update -y;sudo yum -y remove mysql55*;yum list installed>$YUM;
 	for x in php56 mysql56-server;do grep -q $x $YUM||sudo yum -y install $x;done
 	for x in httpd mysqld;do sudo chkconfig $x on;sudo service $x restart;done
+	php --version;mysqld --version;httpd -v
 PHP=$(grep PHP $CONF|cut -d= -f2);sudo sed -i "/memory_limit/s/=.*$/= $PHP/" /etc/php.ini
 BIN=/usr/bin;LOCAL=/usr/local/bin;
 	PACK=composer
 	if [ ! -f $BIN/$PACK ];then
 	URL=https://get$PACK.org/installer 
 	sudo curl -sS https://get$PACK.org/installer|sudo php;
-	sudo mv $PACK.phar $LOCAL/$PACK;sudo ln -s $LOCAL/$PACK $BIN/$PACK
+	sudo mv $PACK.phar $LOCAL/$PACK;sudo ln -s $LOCAL/$PACK $BIN/$PACK;$PACK --version
 	fi
 	PACK=drush
 	if [ ! -f $BIN/$PACK ];then
 	URL=https://github.com/$PACK-ops/$PACK/releases/download/8.1.15/$PACK.phar
 	curl --silent $URL -L --output $PACK.phar;chmod +x $PACK.phar;
-	sudo mv $PACK.phar $LOCAL/$PACK;sudo ln -s $LOCAL/$PACK $BIN/$PACK
+	sudo mv $PACK.phar $LOCAL/$PACK;sudo ln -s $LOCAL/$PACK $BIN/$PACK;$PACK --version
 	fi
 if ! $(grep -q HTTPD $CONF);then
 read -p "HTTPD (default Apache configuration folder: /etc/httpd/conf.d) = " HTTPD
