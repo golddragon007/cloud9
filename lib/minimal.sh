41b595a5cfa50bdb89546550a7c522a299bef367
secobau <sebastian.colomar@gmail.com>
2018-05-02 01:03:44 +0200

    check installed version

--- lib/minimal.sh
+++ lib/minimal.sh
@@ -6,12 +6,12 @@ BIN=/usr/bin;LOCAL=/usr/local/bin;
 	if [ ! -f $BIN/$TOOL ];then
 		curl -L https://github.com/drone/drone-cli/releases/download/v0.8.5/$TOOL_linux_amd64.tar.gz|tar zx;
 		sudo install -t $LOCAL $TOOL;rm $TOOL;
-		sudo ln -s $LOCAL/$TOOL $BIN/$TOOL
+		sudo ln -s $LOCAL/$TOOL $BIN/$TOOL;$TOOL --version
 	fi
 	TOOL=docker-compose;
 	if [ ! -f $BIN/$TOOL ];then
 		COMPOSE=1.21.1
 		URL="https://github.com/docker/compose/releases/download/$COMPOSE/$TOOL-$(uname -s)-$(uname -m)"
 		sudo curl -L $URL -o $LOCAL/$TOOL;sudo chmod +x $LOCAL/$TOOL
-		sudo ln -s $LOCAL/$TOOL $BIN/$TOOL
+		sudo ln -s $LOCAL/$TOOL $BIN/$TOOL;$TOOL --version
 	fi
