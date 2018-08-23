7b24aef1d407eec5066ba4aabc0088decfc3453e
secobau <sebastian.colomar@gmail.com>
2018-08-23 16:31:58 +0200

    apply default value

--- lib/selenium.sh
+++ lib/selenium.sh
@@ -3,7 +3,7 @@ set -e
 LIB=$(dirname "$(readlink -f "$0")");CLOUD=$LIB/..;
 CONFD=$CLOUD/conf.d;CONF=$CONFD/cloud9.conf
 if ! $(grep -q FILE $CONF);then
-read -p "FILE (default Developer configuration file: build.develop.props) = " FILE
+#read -p "FILE (default Developer configuration file: build.develop.props) = " FILE
         FILE=${FILE:-build.develop.props};echo FILE=$FILE>>$CONF
 fi
 if [ ! -f $HOME/environment/cloud9/conf.d/selenium.yml ];then
