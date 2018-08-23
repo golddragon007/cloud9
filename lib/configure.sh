1d941ff0311b4def064e2fa0ea17e51f43011c69
Sébastien GERVAIS <gervais.seb@gmail.com>
2018-08-23 15:07:11 +0000

    Add old script

---
+++ lib/configure.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -x
+set -e
+LIB=$(dirname "$(readlink -f "$0")");CLOUD=$LIB/..;
+CONFD=$CLOUD/conf.d;CONF=$CONFD/$1.conf;DIR=$HOME/environment;
+if [ ! -f $CONF ];then
+read -p "SITE (default name: $1) = " SITE;SITE=${SITE:-"$1"};echo SITE=$SITE>>$CONF
+read -p "REPS (y/n) = " REPS;REPS=${REPS:-n};echo REPS=$REPS>>$CONF
+read -p "VERSION (2.x.y: please change it if the default value 2.3 is not appropriate) = " VERSION;
+	echo VERSION=$VERSION>>$CONF
+read -p "ASDA (password: please leave it blank ONLY for a new subsite) = " ASDA;
+	ASDA=${ASDA:-NOPASSWORD};echo ASDA=$ASDA>>$CONF
+read -p "URL (default https://ec.europa.eu/$SITE) = " URL;URL=${URL:-"https://ec.europa.eu/$SITE"};
+	echo URL=$URL>>$CONF
+REP0=$([ $REPS = y ]&&echo reps-$SITE-reference||echo $SITE-reference);read -p "REPO (default $REP0) = " REPO;
+	REPO=${REPO:-"$REP0"};echo REPO=$REPO>>$CONF
+read -p "GITHUB REPO (https://github.com/ec-europa/$REPO.git change the default if necessary) = " HTTPS;
+	HTTPS=${HTTPS:-"https://github.com/ec-europa/$REPO.git"};echo HTTPS=$HTTPS>>$CONF
+read -p "BRANCH (toolkit/upgrade: change the default if necessary but be careful the branch has toolkit) = " BRANCH;
+	BRANCH=${BRANCH:-"toolkit/upgrade"};echo BRANCH=$BRANCH>>$CONF
+alias drush="$DIR/$REPO/toolkit/drush -r build";echo alias $(alias drush)>>$CONF
+alias phing="$DIR/$REPO/toolkit/phing";echo alias $(alias phing)>>$CONF
+fi
---
+++ lib/system.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+set -e
+LIB=$(dirname "$(readlink -f "$0")");CLOUD=$LIB/..;
+CONFD=$CLOUD/conf.d;[ -d $CONFD ]||mkdir -p $CONFD;CONF=$CONFD/cloud9.conf
+
+[ -f $CONF ]&&mv $CONF $CONF.OLD
+read -p "GITHUB_USER (Github username) = " GITHUB_USER;GITHUB_USER=${GITHUB_USER:-NOUSER};
+	echo GITHUB_USER=$GITHUB_USER>>$CONF
+read -p "GITHUB USER (Name Surname) = " USER;USER=${USER:-NOUSER};echo USER=\"$USER\">>$CONF
+read -p "GITHUB EMAIL (user@domain) = " EMAIL;EMAIL=${EMAIL:-"NOUSER@NOMAIL"};echo EMAIL=$EMAIL>>$CONF
+REGION_ID=${REGION_ID:-eu-west-1};echo REGION_ID=$REGION_ID>>$CONF
+URL=http://169.254.169.254/latest/meta-data/security-groups;
+	ENVIRONMENT_ID=$(curl $URL|awk -F'-InstanceSecurityGroup' '{print $1}'|rev|cut -d- -f1|rev);
+	echo ENVIRONMENT_ID=$ENVIRONMENT_ID;echo ENVIRONMENT_ID=$ENVIRONMENT_ID>>$CONF
+DIR=$HOME/environment;echo DIR=$DIR>>$CONF
+
