54c5267f1cb85017051d557ff32359555dab3f24
secobau <sebastian.colomar@gmail.com>
2018-08-27 10:04:22 +0200

    stop debugging

--- lib/configure.sh
+++ lib/configure.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 set -e
 LIB=$(dirname "$(readlink -f "$0")")
 CLOUD=$LIB/..;
@@ -8,13 +8,13 @@ DIR=$HOME/environment
 if [ ! -f $CONF ];then
 #read -p "SITE (default name: $1) = " SITE;
 	SITE=${SITE:-"$1"};echo SITE=$SITE>>$CONF
-#read -p "REPS (y/n) = " REPS;
+read -p "REPS (y/n) = " REPS;
 	REPS=${REPS:-n};echo REPS=$REPS>>$CONF
-#read -p "VERSION (2.x.y: please change it if the default value $VERSION is not appropriate) = " VERSION;
+read -p "VERSION (2.x.y: please change it if the default value $VERSION is not appropriate) = " VERSION;
 	echo VERSION=$VERSION>>$CONF
 read -p "ASDA (password: please leave it blank ONLY for a new subsite) = " ASDA;
 	ASDA=${ASDA:-NOPASSWORD};echo ASDA=$ASDA>>$CONF
-#read -p "URL (default https://ec.europa.eu/$SITE) = " URL;
+read -p "URL (default https://ec.europa.eu/$SITE) = " URL;
 	URL=${URL:-"https://ec.europa.eu/$SITE"};echo URL=$URL>>$CONF
 REP0=$([ $REPS = y ]&&echo reps-$SITE-reference||echo $SITE-reference);#read -p "REPO (default $REP0) = " REPO;
 	REPO=${REPO:-"$REP0"};echo REPO=$REPO>>$CONF
