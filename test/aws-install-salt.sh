2e308417b04f96b6355a4398862a2874ce1b99e2
gervasek <gervasek@users.noreply.github.com>
2018-09-04 17:47:54 +0200

    Trigger tests only for PR
    Avoid tests for each commit to reduce cost

--- test/aws-install-salt.sh
+++ test/aws-install-salt.sh
@@ -4,6 +4,13 @@
 
 #!/bin/sh -x
 
+# Link local sources
+sudo ln -s /home/ec2-user/pillar /srv/pillar
+sudo ln -s /home/ec2-user/salt /srv/salt
+
+# Create default pillar
+cp /home/ec2-user/pillar/default.sls.sample /home/ec2-user/pillar/default.sls
+
 # Install salt minion: https://repo.saltstack.com/#amzn
 sudo yum install https://repo.saltstack.com/yum/amazon/salt-amzn-repo-latest-2.amzn1.noarch.rpm -y
 sudo yum install salt-minion -y
@@ -13,10 +20,6 @@ echo "aws-test" | sudo tee /etc/salt/minion_id
 
 # Fix: https://github.com/saltstack/salt/issues/47258
 sudo grep -q "service: rh_service" /etc/salt/minion || echo -e "providers:\n  service: rh_service" | sudo tee -a /etc/salt/minion
-
-# Link local sources
-sudo ln -s /home/ec2-user/pillar /srv/pillar
-sudo ln -s /home/ec2-user/salt /srv/salt
 
 # Start minion
 sudo service salt-minion restart
