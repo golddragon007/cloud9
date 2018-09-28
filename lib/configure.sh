ad5878c46a91dc5f0e6bdfb97af4f4080071e474
secobau <sebastian.colomar@gmail.com>
2018-09-28 15:26:03 +0200

    Update configure.sh

--- lib/configure.sh
+++ lib/configure.sh
@@ -8,7 +8,7 @@ CONF=$CONFD/$1.conf
 DIR=$HOME/environment
 VERSION=2.5.62
 if [ ! -f $CONF ];then
-#read -p "SITE (default name: $1) = " SITE;
+read -p "SITE (default name: $1) = " SITE;
 	SITE=${SITE:-"$1"};echo SITE=$SITE>>$CONF
 read -p "REPS (y/n) = " REPS;
 	REPS=${REPS:-n};echo REPS=$REPS>>$CONF
@@ -18,9 +18,9 @@ read -p "ASDA (password: please leave it blank ONLY for a new subsite) = " ASDA;
 	ASDA=${ASDA:-NOPASSWORD};echo ASDA=$ASDA>>$CONF
 read -p "URL (default https://ec.europa.eu/$SITE) = " URL;
 	URL=${URL:-"https://ec.europa.eu/$SITE"};echo URL=$URL>>$CONF
-REP0=$([ $REPS = y ]&&echo reps-$SITE-reference||echo $SITE-reference);#read -p "REPO (default $REP0) = " REPO;
+REP0=$([ $REPS = y ]&&echo reps-$SITE-reference||echo $SITE-reference);read -p "REPO (default $REP0) = " REPO;
 	REPO=${REPO:-"$REP0"};echo REPO=$REPO>>$CONF
-#read -p "GITHUB REPO (https://github.com/ec-europa/$REPO.git change the default if necessary) = " HTTPS;
+read -p "GITHUB REPO (https://github.com/ec-europa/$REPO.git change the default if necessary) = " HTTPS;
 	HTTPS=${HTTPS:-"https://github.com/ec-europa/$REPO.git"};echo HTTPS=$HTTPS>>$CONF
 #read -p "BRANCH (toolkit/upgrade: change the default if necessary but be careful the branch has toolkit) = " BRANCH;
 	BRANCH=${BRANCH:-"toolkit/upgrade"};echo BRANCH=$BRANCH>>$CONF
