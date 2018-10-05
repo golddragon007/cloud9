f69b7babbce70ae7f5e9c052cfe321b580e03c3a
Sébastien GERVAIS <gervais.seb@gmail.com>
2018-10-05 15:24:44 +0000

    Update lambda_function.py to manage security groups

--- lambda_function.py
+++ lambda_function.py
@@ -1,78 +1,59 @@
+from __future__ import print_function
+
+import json
 import boto3
-ec2R = boto3.resource('ec2')
-ec2 = boto3.client('ec2')
-c9 = boto3.client('cloud9')
-from botocore.exceptions import ClientError
 
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-debugMode = False
+sgCommon = "sg-0e7229b92b492ad7c"
 
-DRYRUN = False
-if (debugMode):
-  logger.setLevel(logging.DEBUG)
-  DRYRUN = True
-
-EBS_SIZE = 20
-
+client = boto3.client('ec2')
+ec2R = boto3.resource('ec2')
+      
 def lambda_handler(event, context):
+  
   if (event['detail']['event'] == "createVolume" ):
     if (event['detail']['result'] != "available"):
       logger.error("Volume not ready")
+  else:
+    #logger.info("EC {0} start maange security-group".format(ec2instance.id) )
+    logger.info("Volume ready")
+    
+    # Get volume from event
+    ressources = event['resources'][0]
+    arn,volumeID = ressources.split(":volume/")
+    volume = ec2R.Volume(volumeID)
+    
+    # Get EC2 attached instance ID 
+    if  len(volume.attachments) == 0:
+      logger.info("Volume {0} not attached to an EC2 instance".format(volumeID))
+      return False
     else:
-      # Get volume from event
-      ressources = event['resources'][0]
-      arn,volumeID = ressources.split(":volume/")
-      volume = ec2R.Volume(volumeID)
-      logger.info("Volume {0} start resize".format(volumeID) )
+      EC2Id=volume.attachments[0]['InstanceId']
+      ec2instance = ec2R.Instance(EC2Id)
+      #logger.info("EC {0} start maange security-group".format(ec2instance.id) )
+      logger.info("EC instance found: {0}. Start manage security-group".format(ec2instance.id))
       
-      # Get EC2 attached instance ID 
-      if  len(volume.attachments) == 0:
-        logger.info("Volume {0} skipped, not attached to an EC2 instance".format(volumeID))
-        return False
-      else:
-        EC2Id=volume.attachments[0]['InstanceId']
-        ec2instance = ec2R.Instance(EC2Id)
-      
-      # Ensure it's volume of cloud9 environment
-      isC9=False
-      for tags in ec2instance.tags:
-        if tags["Key"] == 'aws:cloud9:environment':
-          isC9 = True
-          # if 'noebsresize' is present on description, don't resize volume
-          response = c9.describe_environments(environmentIds=[tags["Value"]])
-          c9env  = response['environments'][0]
-          if "noebsresize" in c9env['description'].lower():
-            logger.info("Volume {0} skipped: 'noebsresize' found on C9 description".format(volumeID))
-            return False
-          break;
-          
-      if isC9:
-        curSize = volume.size
-        if curSize >= EBS_SIZE:
-        # Size Ok, skip
-          logger.info("Volume {0} skipped: size OK: {1}Go".format(volumeID, curSize) )
+      all_sg_ids = [sg['GroupId'] for sg in ec2instance.security_groups]  # Get a list of ids of all securify groups attached to the instance
+      if sgCommon not in all_sg_ids:
+        # Assign only the common security group.
+        response = ec2instance.modify_attribute(Groups=[sgCommon])
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+          logger.info("EC {0} common security group assigned: {1}".format(ec2instance.id, sgCommon))
         else:
-        # Edit Volume
-          response = False
-          try:
-            response = ec2.modify_volume(
-                DryRun=DRYRUN,
-                VolumeId=volumeID,
-                Size=EBS_SIZE
-            )
-          except ClientError as e:
-            logger.debug(e)
-            #An error occurred (VolumeModificationRateExceeded) when calling the ModifyVolume operation: You've reached the maximum modification rate per volume limit. Wait at least 6 hours between modifications per EBS volume.
-            logger.error("Volume {0} failed: ClientError Exception: {1}".format(volumeID, e))
-            return False
-          if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            logger.info("Volume {0} updated: size changed from {1}Go to {2}Go".format(volumeID, curSize, EBS_SIZE))
-            return True
-          else:
-            logger.error("Volume {0} failed: Target {1}Go, Current Size: {2}. Responde: {3}".format(volumeID, EBS_SIZE,curSize, response))
+          logger.error("EC {0} error when trying to assign common security group: {1}. Responde: {2}".format(ec2instance.id, sgCommon, response))
       else:
-        logger.info("Volume {0} skipped: not cloud9 environment".format(volumeID))
-  return False
+        logger.info("EC {0} assign common security group {1} skipped: already attached.".format(ec2instance.id, sgCommon) )
+        
+      for sg_id in all_sg_ids :
+        response = client.delete_security_group(GroupId=sg_id)
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+          logger.info("EC {0} old security group {1} deleted.".format(ec2instance.id, sg_id))
+        else:
+          logger.error("EC {0} error when trying to delete old security group {1}. Responde: {2}".format(ec2instance.id, sg_id, response))
+          
+    logger.info("EC {0} manage security-group finished".format(ec2instance.id))
+    
+    return True
