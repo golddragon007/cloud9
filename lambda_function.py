c6c1552b701bfcac8e9b290be749957414e1b64e
EC2 Default User <ec2-user@ip-172-31-43-56.eu-west-1.compute.internal>
2018-10-22 08:37:48 +0000

    FPFISSUPP-3012 : add ec2 unused alert script

--- lambda_function.py
+++ lambda_function.py
@@ -1,79 +1,79 @@
+import json
 import boto3
-import requests
+import datetime
+import os
 import ConfigParser
-import os.path
+from botocore.vendored import requests
 
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+ec2_res = boto3.resource('ec2')
+accountName = "Test"
+if "Account" in os.environ.keys():
+    accountName = os.environ['Account']
+
+
 def lambda_handler(event, context):
-  cloud9 = boto3.client('cloud9')
-  cloud9All = {}
-  Cloud9ByOwners = {}
-  hasNextToken  = True
-  nextTokenValue = ''
-  while hasNextToken == True:
-    result = cloud9.list_environments(nextToken=nextTokenValue)
-    if 'nextToken' in result:
-      nextTokenValue = result['nextToken']
+#####Get EC2
+    #Filter get stopped ec2
+    filters = [
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['stopped']
+        }
+    ]
+    #Set date - 2 month
+    start_date = datetime.datetime.today() - datetime.timedelta(days=60)
+    print("Start " + start_date.strftime("%A %d. %B %Y"))
+    
+    #Get instances by filters
+    instances = ec2_res.instances.filter(Filters=filters)
+    ec2_list = []
+    
+    count = 0
+    message = ""
+    #Parse all instances which are running or stopped
+    for instance in instances :
+        if(instance.launch_time.replace(tzinfo=None) < start_date):
+            count = count + 1
+            name = ""
+            for j in range(len(instance.tags)):
+                if instance.tags[j]["Key"] == "Name":
+                    name = instance.tags[j]["Value"]
+            instance_description  = " --> " + name + " (" + instance.id + " / " + str(instance.launch_time.date()) + ") : " + instance.instance_type + "\n"
+            logger.info(instance_description)
+            message = message + instance_description
+    
+    message = "These " + str(count) + " instances have been stopped since more than 2 month :\n" + message
+    message = "########### ACCOUNT : " + accountName + " ###########\n" + message
+    
+    print(message)
+    logger.debug ("Message: {0}".format(message))
+    logger.info ("Send slack message...")
+    result = sendSlackMessage(message)
+    
+    if result == 200:
+        logger.info ("Slack notification sent.")
     else:
-      hasNextToken = False
-     
-    # Get Environements descriptions
-    environmentsDescribe = cloud9.describe_environments(environmentIds=result['environmentIds'])
-
-    # Loop on each environement
-    for environmentDescribe in environmentsDescribe['environments']:
-      ownerArn=environmentDescribe['ownerArn']
-      cloud9Id=environmentDescribe['id']
-      cloud9All[cloud9Id] = environmentDescribe
-      
-      # Create list by owner
-      if Cloud9ByOwners.get(ownerArn, 0) == 0:
-          Cloud9ByOwners[ownerArn]=[]
-      Cloud9ByOwners[ownerArn].append(cloud9Id)
-  
-  # Check for multiple env by User
-  message=""
-  for Cloud9ByOwner in Cloud9ByOwners: 
-      if len(Cloud9ByOwners[Cloud9ByOwner]) > 1:
-        import re
-        m = re.search(r'arn:aws:iam:.*:user/(.*)', Cloud9ByOwner)
-        username=m.group(1)
-        env=[]
-        for cloud9ID in Cloud9ByOwners[Cloud9ByOwner]: 
-          env.append (cloud9All[cloud9ID]["name"])
-        
-        message += unichr(8226) + " User '%s' have %s environements: %s\n" % (username,len(Cloud9ByOwners[Cloud9ByOwner]),', '.join(env))
-        logger.info ("User '{0}' have multiple environments: {1}".format(username,', '.join(env)))
-  message = "Only 1 Cloud9 environment is allowed by user. Please clean up your environments:\n" + message
-  logger.debug ("Message: {0}".format(message))
-  logger.info ("Send slack message...")
-  result = sendSlackMessage(message)
-  
-  if result == 200:
-    logger.info ("Slack notification sent.")
-  else:
-    logger.error ("Slack notification error, return code: %d" % (result))
-  
-
+        logger.error ("Slack notification error, return code: %d" % (result))
+    
 def sendSlackMessage (message) :
-  CONFIGFILE=  os.path.dirname(__file__) + "/lambda_config.ini"
-  if not os.path.isfile(CONFIGFILE):
-    logger.error("Config file not fount at '{0}'".format(CONFIGFILE))
-    return False
-  config = ConfigParser.ConfigParser()
-  config.read(CONFIGFILE)
-  try:
-    SLACK_URL=config.get('slack', 'webhook_URL')
-    SLACK_CHANNEL=config.get('slack', 'channel')
-    SLACK_USERNAME=config.get('slack', 'username')
-    SLACK_ICON = config.get('slack', 'icon_emoji')
-    response = { "text" : message, "channel": SLACK_CHANNEL, "username": SLACK_USERNAME, "icon_emoji": SLACK_ICON}
-    slack_response = requests.post(SLACK_URL,json=response, headers={'Content-Type': 'application/json'})
-    return slack_response.status_code
-  except Exception as inst:
-    logger.error ("Slack Callout Failed.Message: '%s'" % (inst))
-    return 0
-        
+    CONFIGFILE=  os.path.dirname(__file__) + "/lambda_config.ini"
+    if not os.path.isfile(CONFIGFILE):
+        logger.error("Config file not fount at '{0}'".format(CONFIGFILE))
+        return False
+    config = ConfigParser.ConfigParser()
+    config.read(CONFIGFILE)
+    try:
+        SLACK_URL=config.get('slack', 'webhook_URL')
+        SLACK_CHANNEL=config.get('slack', 'channel')
+        SLACK_USERNAME=config.get('slack', 'username')
+        SLACK_ICON = config.get('slack', 'icon_emoji')
+        response = { "text" : message, "channel": SLACK_CHANNEL, "username": SLACK_USERNAME, "icon_emoji": SLACK_ICON}
+        slack_response = requests.post(SLACK_URL,json=response, headers={'Content-Type': 'application/json'})
+        return slack_response.status_code
+    except Exception as inst:
+        logger.error ("Slack Callout Failed.Message: '%s'" % (inst))
+        return 0
