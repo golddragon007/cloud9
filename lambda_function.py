2afdb8aa8d26507449a63c95cc25d0e30f772684
EC2 Default User <ec2-user@ip-172-31-43-56.eu-west-1.compute.internal>
2018-10-22 09:44:11 +0000

    FPFISSUPP-3012 : add parse of the EU regions

--- lambda_function.py
+++ lambda_function.py
@@ -9,55 +9,73 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-ec2_res = boto3.resource('ec2')
+ec2_cli = boto3.client('ec2')
 accountName = "Test"
 if "Account" in os.environ.keys():
     accountName = os.environ['Account']
+    
+#List european zone
+eu_region = []
+regions = ec2_cli.describe_regions()["Regions"]
+for region in regions:
+    if ("eu-" in region["RegionName"]) :
+        eu_region.append(region["RegionName"])
 
 
 def lambda_handler(event, context):
-#####Get EC2
-    #Filter get stopped ec2
-    filters = [
-        {
-            'Name': 'instance-state-name', 
-            'Values': ['stopped']
-        }
-    ]
+    
+    count = 0
+    message = ""
+    
     #Set date - 2 month
     start_date = datetime.datetime.today() - datetime.timedelta(days=60)
     print("Start " + start_date.strftime("%A %d. %B %Y"))
     
-    #Get instances by filters
-    instances = ec2_res.instances.filter(Filters=filters)
-    ec2_list = []
+    for region in eu_region :
+        
+        ec2_res = boto3.resource('ec2', region_name = region)
+        #####Get EC2
+        #Filter get stopped ec2
+        filters = [
+            {
+                'Name': 'instance-state-name', 
+                'Values': ['stopped']
+            }
+        ]
+        
+        #Get instances by filters
+        instances = ec2_res.instances.filter(Filters=filters)
+        ec2_list = []
+        
+        regionReport = ""
+        #Parse all instances which are running or stopped
+        for instance in instances :
+            if(instance.launch_time.replace(tzinfo=None) < start_date):
+                count = count + 1
+                name = ""
+                for j in range(len(instance.tags)):
+                    if instance.tags[j]["Key"] == "Name":
+                        name = instance.tags[j]["Value"]
+                instance_description  = " --> " + name + " (" + instance.id + " / " + str(instance.launch_time.date()) + ") : " + instance.instance_type + "\n"
+                logger.info(instance_description)
+                regionReport = regionReport + instance_description
+        if not regionReport == "":
+            message = "### " + region + " \n" + regionReport 
     
-    count = 0
-    message = ""
-    #Parse all instances which are running or stopped
-    for instance in instances :
-        if(instance.launch_time.replace(tzinfo=None) < start_date):
-            count = count + 1
-            name = ""
-            for j in range(len(instance.tags)):
-                if instance.tags[j]["Key"] == "Name":
-                    name = instance.tags[j]["Value"]
-            instance_description  = " --> " + name + " (" + instance.id + " / " + str(instance.launch_time.date()) + ") : " + instance.instance_type + "\n"
-            logger.info(instance_description)
-            message = message + instance_description
+    if not message == "" :
+        message = "These " + str(count) + " instances have been stopped since more than 2 month :\n" + message
+        message = "########### ACCOUNT : " + accountName + " ###########\n" + message
     
-    message = "These " + str(count) + " instances have been stopped since more than 2 month :\n" + message
-    message = "########### ACCOUNT : " + accountName + " ###########\n" + message
-    
-    print(message)
-    logger.debug ("Message: {0}".format(message))
-    logger.info ("Send slack message...")
-    result = sendSlackMessage(message)
-    
-    if result == 200:
-        logger.info ("Slack notification sent.")
-    else:
-        logger.error ("Slack notification error, return code: %d" % (result))
+        print(message)
+        logger.debug ("Message: {0}".format(message))
+        logger.info ("Send slack message...")
+        #result = sendSlackMessage(message)
+        result = 200
+        
+        if result == 200:
+            logger.info ("Slack notification sent.")
+        else:
+            logger.error ("Slack notification error, return code: %d" % (result))
     
 def sendSlackMessage (message) :
     CONFIGFILE=  os.path.dirname(__file__) + "/lambda_config.ini"
