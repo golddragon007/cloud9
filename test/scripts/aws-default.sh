960bd6f236878417f06c4604615ff14b731a92e4
gervasek <gervasek@users.noreply.github.com>
2018-12-05 15:09:15 +0100

    Test for automaticShutdown java script (#73)

--- test/scripts/aws-default.sh
+++ test/scripts/aws-default.sh
@@ -18,8 +18,32 @@ sudo salt-call --retcode-passthrough state.apply tools.nodejs --local
 
 sudo salt-call --retcode-passthrough state.apply frp --local
 
-# Check 
+#################################
+###   Test git & bash alias   ###
+#################################
 git st || if (( "$?" == 1));then exit 1;fi
 source ~/.bashrc
 cdenv
 node --version
+
+######################################
+###   Test Java autoshutdown app   ###
+######################################
+
+# Ensure service is started at c9 boot
+if ! sudo /etc/init.d/cronp status; then echo "Cronp status failed, exit..." ; exit 22; fi
+# Test replacement at service start
+sudo /etc/init.d/cronp stop
+sed -i -e 's/"@automaticShutdown".*:.*".*"/"@automaticShutdown":"66"/g' /home/ec2-user/environment/.c9/project.settings
+sudo /etc/init.d/cronp start
+if ! egrep '@automaticShutdown".*:.*"30"' /home/ec2-user/environment/.c9/project.settings; then echo "Change on project.settings fail, exit..." ; exit 23; fi
+# Test replacement on live
+sed -i -e 's/"@automaticShutdown".*:.*".*"/"@automaticShutdown":"66"/g' /home/ec2-user/environment/.c9/project.settings
+sleep 2
+if ! egrep '@automaticShutdown".*:.*"30"' /home/ec2-user/environment/.c9/project.settings; then echo "Change on project.settings fail, exit..." ; exit 24; fi
+# Test java don't fail after second replacement
+sed -i -e 's/"@automaticShutdown".*:.*".*"/"@automaticShutdown":"66"/g' /home/ec2-user/environment/.c9/project.settings
+sleep 2
+if ! egrep '@automaticShutdown".*:.*"30"' /home/ec2-user/environment/.c9/project.settings; then echo "Change on project.settings fail, exit..." ; exit 25; fi
+if ! sudo /etc/init.d/cronp status; then echo "Cronp status failed, exit..." ; exit 26; fi
+
